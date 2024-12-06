ea64a98269ebd191d26c4b2f7dcee8ded7391b22
MichalOleniacz <oleniaczmichal852@gmail.com>
2024-12-06 23:07:15 +0100

    test: HibernateUtil tests

---
+++ src/test/java/org/michaloleniacz/lab/utils/HibernateUtilTest.java
@@ -0,0 +1,31 @@
+package org.michaloleniacz.lab.utils;
+
+import org.hibernate.Session;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class HibernateUtilTest {
+
+    @Test
+    @DisplayName("HibernateUtil should throw when initialized with invalid path")
+    public void initializeWithInvalidPath() {
+        final String invalidPath = "foo";
+        assertThatExceptionOfType(ExceptionInInitializerError.class)
+                .isThrownBy(() -> HibernateUtil.initialize(invalidPath));
+    }
+
+    @Test
+    @DisplayName("HibernateUtil should autoinitialize using fallback path when getting instance")
+    public void getSessionAutoInitialize() {
+        HibernateUtil.closeSession();
+
+        Session session = HibernateUtil.getSession();
+
+        assertThat(session).isInstanceOf(Session.class);
+    }
+}
